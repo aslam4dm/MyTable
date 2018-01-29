f150557c6727dcc123f9e660bf4151a1aeaa17ae
jumb01 <35896884+jumb01@users.noreply.github.com>
2018-01-29 23:43:36 +0000

    Update MyTable.py
    Fresh code! New and efficient ideas came to mind to optimize code by 98%

--- Source/MyTable.py
+++ Source/MyTable.py
@@ -1,8 +1,6 @@
-#coded by jumb0
+# coded by jumb0
 from col import colours as C
-#apologies for the ugliness of my code! ¬.¬
 
-# main class
 class Table(object):
 
 	def __init__(self, data, column):
@@ -21,16 +19,31 @@ class Table(object):
 				if i % self.column == 0:
 					self.row += 1
 
+		self.borders = C.darkgrey; self.text = C.cyan
+		self.defaultStyle = ["|", "-", "+"]
+		self.defaultSpacing = 6
+
 	def title(self, t):
 		print("-"*self.column + "[", t, "]" + "-"*self.column)
 
-	def style1(self):
+	def colour(self, borderC, textC):
+		self.borders = borderC
+		self.text = textC
+
+	def style(self, *args):
+		if len(args) > 2:
+			self.defaultStyle = [args[0], args[1], args[2]]
+		else:
+			self.defaultStyle = [args[0], args[1], "+"]
+
+	def make_table(self):
+		self.defaultSpacing = 6
 		for n in range(self.column):
 			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "-"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
 			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "-"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+".format(C.darkgrey))
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
 		pipelines = 0
 		counter = 0
 		counter2 = 1
@@ -43,9 +56,9 @@ class Table(object):
 				if counter % self.column == 0 and counter > 0:
 					print("")
 				if counter+1 < 10:
-					print("{}| {}".format(C.darkgrey, C.end), "{}{}.{} {}{}{}".format(C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
+					print("{}{}{} ".format(self.borders, self.defaultStyle[0], C.end), "{}{}.{} {}{}{}".format(C.green, counter+1, C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
 				else:
-					print("{}|{} {}{}.{} {}{}{}".format(C.darkgrey, C.end, C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
+					print("{}{}{} {}{}.{} {}{}{}".format(self.borders, self.defaultStyle[0], C.end, C.green, counter+1, C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
 				pipelines+=2
 			except:
 				pass
@@ -57,23 +70,24 @@ class Table(object):
 			oP = P*c 			# overall pipelines (that should exist)
 			r = oP - pipelines 	# num of remainder (pipelines missing)
 			for i in range(r//2):
-				#done don't change!
-				print("{}|{}".format(C.darkgrey, C.end), " "*(6+self.Max_Length),  "{}|{}".format(C.darkgrey, C.end), end="")
+				print("{}{}".format(self.borders, self.defaultStyle[0]), " "*(self.defaultSpacing+self.Max_Length),  "{}{}".format(self.defaultStyle[0], C.end), end="")
 		print("")
 		for n in range(self.column):
 			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "-"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
 			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "-"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
 
-	def style2(self):
+
+	def numberless_table(self):
+		self.defaultSpacing = 3
 		for n in range(self.column):
 			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "="*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
 			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "="*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
 		pipelines = 0
 		counter = 0
 		counter2 = 1
@@ -86,229 +100,9 @@ class Table(object):
 				if counter % self.column == 0 and counter > 0:
 					print("")
 				if counter+1 < 10:
-					print("{}| {}".format(C.darkgrey, C.end), "{}{}{}. {}{}{}".format(C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
+					print("{}{}{} ".format(self.borders, self.defaultStyle[0], C.end), "{}{}{}".format(self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
 				else:
-					print("{}| {}{}{}.{} {}{}{}".format(C.darkgrey, C.end, C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	#column
-			R = self.row		# row
-			P = R*2				# num of pipelines
-			oP = P*c 			# overall pipelines (that should exist)
-			r = oP - pipelines 	# num of remainder (pipelines missing)
-			for i in range(r//2):
-				#done don't change!
-				print("{}|{}".format(C.darkgrey, C.end), " "*(6+self.Max_Length),  "{}|{}".format(C.darkgrey, C.end), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "="*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "="*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
-
-
-	def style3(self):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "~"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "~"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				if counter+1 < 10:
-					print("{}| {}".format(C.darkgrey, C.end), "{}{}{}. {}{}{}".format(C.lightgrey, C.end, counter+1, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				else:
-					print("{}|{} {}{}.{} {}{}{}".format(C.darkgrey, C.end, C.lightgrey, C.end, counter+1, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	#column
-			R = self.row		# row
-			P = R*2				# num of pipelines
-			oP = P*c 			# overall pipelines (that should exist)
-			r = oP - pipelines 	# num of remainder (pipelines missing)
-			for i in range(r//2):
-				#done don't change!
-				print("{}|{}".format(C.darkgrey, C.end), " "*(6+self.Max_Length),  "{}|{}".format(C.darkgrey, C.end), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "~"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("+{}".format(C.darkgrey) + "~"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
-
-
-	def style4(self):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + ">"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}X".format(C.darkgrey) + ">"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}X{}".format(C.darkgrey, C.end))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				if counter+1 < 10:
-					print("{}| {}".format(C.darkgrey, C.end), "{}{}{}. {}{}{}".format(C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				else:
-					print("{}|{} {}{}.{} {}{}".format(C.darkgrey, C.end, C.lightgrey, counter, C.end, C.red, i, C.end), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	#column
-			R = self.row		# row
-			P = R*2				# num of pipelines
-			oP = P*c 			# overall pipelines (that should exist)
-			r = oP - pipelines 	# num of remainder (pipelines missing)
-			for i in range(r//2):
-				#done don't change!
-				print("{}|{}".format(C.darkgrey, C.end), " "*(6+self.Max_Length),  "{}|{}".format(C.darkgrey, C.end), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "<"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}X".format(C.darkgrey) + "<"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}X{}".format(C.darkgrey, C.end))
-
-
-	def style5(self):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "$"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}$".format(C.darkgrey) + "$"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}${}".format(C.darkgrey, C.end))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				if counter+1 < 10:
-					print("{}${} ".format(C.darkgrey, C.end), "{}{}.{} {}{}{}".format(C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}${}".format(C.darkgrey, C.end), end="",)
-				else:
-					print("{}${} {}{}.{} {}{}{}".format(C.darkgrey, C.end, C.lightgrey, counter+1, C.end, C.red, i, C.end), " "*self.addon, "{}${}".format(C.darkgrey, C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	#column
-			R = self.row		# row
-			P = R*2				# num of pipelines
-			oP = P*c 			# overall pipelines (that should exist)
-			r = oP - pipelines 	# num of remainder (pipelines missing)
-			for i in range(r//2):
-				#done don't change!
-				print("{}${}".format(C.darkgrey, C.end), " "*(6+self.Max_Length),  "{}${}".format(C.darkgrey, C.end), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "$"*(6+self.Max_Length+3)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}$".format(C.darkgrey) + "$"*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}${}".format(C.darkgrey, C.end))
-
-
-# [  non numbered styles ]
-# default char value of 4, && equal spaces
-# with non numbered styles, the list index doesn't matter
-# because number spaces are disregarded, thus each element
-# prints with 2 whitespace, see line 18 in style6
-
-	def style6(self):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "~"*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "~"*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+".format(C.darkgrey))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				print("{}|{}  {}{}".format(C.darkgrey, C.end, C.red, i), " "*self.addon, "{}|{}".format(C.darkgrey, C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	#column
-			R = self.row		# row
-			P = R*2				# num of pipelines
-			oP = P*c 			# overall pipelines (that should exist)
-			r = oP - pipelines 	# num of remainder (pipelines missing)
-			for i in range(r//2):
-				#done don't change!
-				print("{}|".format(C.darkgrey), " "*(3+self.Max_Length),  "{}|".format(C.darkgrey), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "~"*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}+".format(C.darkgrey) + "~"*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}+{}".format(C.darkgrey, C.end))
-
-
-	def style7(self):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "."*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}o".format(C.darkgrey) + "."*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}o{}".format(C.darkgrey, C.end))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				print("{}o{}  {}{}{}".format(C.darkgrey, C.end, C.red, i, C.end), " "*self.addon, "{}o{}".format(C.darkgrey, C.end), end="",)
+					print("{}{}{}  {}{}{}".format(self.borders, self.defaultStyle[0], C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
 				pipelines+=2
 			except:
 				pass
@@ -320,55 +114,11 @@ class Table(object):
 			oP = P*c 			
 			r = oP - pipelines 	
 			for i in range(r//2):
-				#done don't change!
-				print("{}o".format(C.darkgrey), " "*(3+self.Max_Length),  "o{}".format(C.end), end="")
+				print("{}{}".format(self.borders, self.defaultStyle[0]), " "*(self.defaultSpacing+self.Max_Length),  "{}{}".format(self.defaultStyle[0], C.end), end="")
 		print("")
 		for n in range(self.column):
 			if (n > 0) and (n < self.column):
-				print("{}".format(C.darkgrey) + "."*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
 			elif (n == 0 or n+1 == self.column):
-				print("{}o".format(C.darkgrey) + "."*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}o{}".format(C.darkgrey, C.end))
-
-
-# with colour, requires 2 arguments
-# arg1 = border colour; arg2 = text colour
-	def style8(self, *args):
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(args[0]) + ">"*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}%".format(args[0]) + ">"*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}%{}".format(args[0], C.end))
-		pipelines = 0
-		counter = 0
-		counter2 = 1
-		for i in self.data:
-			self.addon = 1
-			try:
-				while len(i)+self.addon != self.Max_Length+1: 
-					self.addon += 1
-					continue
-				if counter % self.column == 0 and counter > 0:
-					print("")
-				print("{}%{}  {}{}{}".format(args[0], C.end, args[1], i, C.end), " "*self.addon, "{}%{}".format(args[0], C.end), end="",)
-				pipelines+=2
-			except:
-				pass
-			counter += 1; counter2 += 1
-		if len(self.data) % self.column != 0:
-			c = self.column 	
-			R = self.row		
-			P = R*2				
-			oP = P*c 			
-			r = oP - pipelines 	
-			for i in range(r//2):
-				#done don't change!
-				print("{}%".format(args[0]), " "*(3+self.Max_Length),  "%{}".format(C.end), end="")
-		print("")
-		for n in range(self.column):
-			if (n > 0) and (n < self.column):
-				print("{}".format(args[0]) + "<"*(4+self.Max_Length+2)+ "{}".format(C.end), end="",)
-			elif (n == 0 or n+1 == self.column):
-				print("{}%".format(args[0]) + "<"*(4+self.Max_Length+2+(self.column-2))+ "{}".format(C.end), end="",)
-		print("{}%{}".format(args[0], C.end))
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
